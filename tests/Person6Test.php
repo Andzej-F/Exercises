1dc1f5ed7e3ef375305889242211be3eb2d8c764
Andzej-F <70884391+Andzej-F@users.noreply.github.com>
2021-09-14 13:13:22 +0300

    .

--- tests/Person6Test.php
+++ tests/Person6Test.php
@@ -25,50 +25,312 @@ final class Person6Test extends TestCase
         $this->assertTrue((new ReflectionProperty('Person6', 'age'))->isProtected());
         $this->assertTrue((new ReflectionProperty('Person6', 'occupation'))->isProtected());
     }
-    /*
-    public function testIfNameIsAStringException()
+
+    public function testInvalidName1()
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Name must be a string!");
-        $john = new Person6(56, 26, 'Driver');
+        new Person6(3, 18, "Web Developer");
     }
 
-    public function testIfAgeIsAPositiveIntegerException()
+    public function testInvalidName2()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Name must be a string!");
+        new Person6(true, 18, "Web Developer");
+    }
+
+    public function testInvalidName3()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Name must be a string!");
+        new Person6(M_PI, 18, "Web Developer");
+    }
+
+    public function testInvalidName4()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Name must be a string!");
+        new Person6([1, 2, 3], 18, "Web Developer");
+    }
+
+    public function testInvalidName5()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Name must be a string!");
+        new Person6(false, 18, "Web Developer");
+    }
+
+    public function testInvalidAge1()
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Age must be a non-negative integer!");
-        $sylvia = new Person6('Sylvia', '-56', 'Driver');
+        new Person6('Rita Carr', -1, "Web Developer");
     }
 
-    public function testIfOccupationIsAStringException()
+    public function testInvalidAge2()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Age must be a non-negative integer!");
+        new Person6('Rita Carr', 17.53, "Web Developer");
+    }
+
+    public function testInvalidAge3()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Age must be a non-negative integer!");
+        new Person6('Rita Carr', -17.53, "Web Developer");
+    }
+
+    public function testInvalidAge4()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Age must be a non-negative integer!");
+        new Person6('Rita Carr', false, "Web Developer");
+    }
+
+    public function testInvalidAge5()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Age must be a non-negative integer!");
+        new Person6('Rita Carr', "age", "Web Developer");
+    }
+
+    public function testInvalidOccupation1()
     {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Occupation must be a string!");
-        $joanna = new Person6('Joanna', 26, 47);
+        new Person6('Rita Carr', 17, 17);
     }
 
-    public function testSalesman6ObjectConstructor()
+    public function testInvalidOccupation2()
     {
-        $ana = new Person6('Ana', '26', 'Designer');
-        $this->assertEquals('Ana', $ana->get_name());
-        $this->assertEquals('26', $ana->get_age());
-        $this->assertEquals('Designer', $ana->get_occupation());
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Occupation must be a string!");
+        new Person6('Rita Carr', 17, M_E);
     }
 
-    public function testSalesman6ObjectConstructorWithToken()
+    public function testInvalidOccupation3()
     {
-        $weirdName = new Person6('ne4g26lwpf88914wkfuk', 86, 'Designer');
-        $this->assertEquals('ne4g26lwpf88914wkfuk', $weirdName->get_name());
-        $this->assertEquals(86, $weirdName->get_age());
-        $this->assertEquals('s425xtl79zeel12loy94', $weirdName->get_occupation());
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Occupation must be a string!");
+        new Person6('Rita Carr', 17, true);
+    }
+
+    public function testInvalidOccupation4()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Occupation must be a string!");
+        new Person6('Rita Carr', 17, false);
+    }
+
+    public function testInvalidOccupation5()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Occupation must be a string!");
+        new Person6('Rita Carr', 17, ["First Program" => "Hello World"]);
+    }
+
+    protected function randomToken()
+    {
+        $result = "";
+        for ($i = 0; $i < 20; $i++) {
+            $result .= "abcdefghijklmnopqrstuvwxyz0123456789"[rand(0, 35)];
+        }
+
+        return $result;
+    }
+
+    public function testGetters()
+    {
+        // Fixed Assertions
+        $donald = new Person6("Donald", 17, "student");
+        $this->assertEquals('Donald', $donald->get_name());
+        $this->assertEquals(17, $donald->get_age());
+        $this->assertEquals('student', $donald->get_occupation());
+
+        $chris = new Person6("Chris", 25, "Computer Programmer");
+        $this->assertEquals('Chris', $chris->get_name());
+        $this->assertEquals(25, $chris->get_age());
+        $this->assertEquals('Computer Programmer', $chris->get_occupation());
+
+        $alexandra = new Person6("Alexandra", 20, "Web Developer");
+        $this->assertEquals('Alexandra', $alexandra->get_name());
+        $this->assertEquals(20, $alexandra->get_age());
+        $this->assertEquals('Web Developer', $alexandra->get_occupation());
+
+        //Random Assertions
+        for ($i = 0; $i < 100; $i++) {
+            $name = $this->randomToken();
+            $age = rand(0, 100);
+            $occupation = $this->randomToken();
+            $person = new Person6($name, $age, $occupation);
+
+            $this->assertEquals($name, $person->get_name());
+            $this->assertEquals($age, $person->get_age());
+            $this->assertEquals($occupation, $person->get_occupation());
+        }
+    }
+
+    public function testSettersWithValidInput()
+    {
+        $person = new Person6("Antonio McDyess", 17, "Computer Programmer");
+        $this->assertEquals("Antonio McDyess", $person->get_name());
+        $this->assertEquals(17, $person->get_age());
+        $this->assertEquals("Computer Programmer", $person->get_occupation());
+
+        // Fixed Assertions
+        $person->set_name("Lincoln");
+        $person->set_age(16);
+        $person->set_occupation("Politician");
+        $this->assertEquals("Lincoln", $person->get_name());
+        $this->assertEquals(16, $person->get_age());
+        $this->assertEquals("Politician", $person->get_occupation());
+
+        $person->set_name("Ella");
+        $person->set_age(18);
+        $person->set_occupation("Doctor");
+        $this->assertEquals("Ella", $person->get_name());
+        $this->assertEquals(18, $person->get_age());
+        $this->assertEquals("Doctor", $person->get_occupation());
+
+        $person->set_name("Jon");
+        $person->set_age(25);
+        $person->set_occupation("Lawyer");
+        $this->assertEquals("Jon", $person->get_name());
+        $this->assertEquals(25, $person->get_age());
+        $this->assertEquals("Lawyer", $person->get_occupation());
+
+        // Random Assertions
+        for ($i = 0; $i < 100; $i++) {
+            $person->set_name($name = $this->randomToken());
+            $person->set_age($age = rand(0, 100));
+            $person->set_occupation($occupation = $this->randomToken());
+            $this->assertEquals($name, $person->get_name());
+            $this->assertEquals($age, $person->get_age());
+            $this->assertEquals($occupation, $person->get_occupation());
+        }
+    }
+
+    public function testSetterWithInvalidName1()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Name must be a string!');
+        $person = new Person6('Donal Leung', 17, "Computer Programmer");
+        $person->set_name(34);
+    }
+
+    public function testSetterWithInvalidName2()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Name must be a string!');
+        $person = new Person6('Donal Leung', 17, "Computer Programmer");
+        $person->set_name(-373);
+    }
+
+    public function testSetterWithInvalidName3()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Name must be a string!');
+        $person = new Person6('Donal Leung', 17, "Computer Programmer");
+        $person->set_name(-M_PI);
+    }
+
+    public function testSetterWithInvalidName4()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Name must be a string!');
+        $person = new Person6('Donal Leung', 17, "Computer Programmer");
+        $person->set_name(true);
+    }
+
+    public function testSetterWithInvalidName5()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Name must be a string!');
+        $person = new Person6('Donal Leung', 17, "Computer Programmer");
+        $person->set_name(NULL);
     }
 
     public function testSetterWithInvalidAge1()
     {
-        $james = new Person6('James', '32', 'Forester');
-        // $this->expectException(InvalidArgumentException::class);
-        $james->set_age(52);
-        $this->assertEquals(52, $james->get_age());
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Age must be a non-negative integer!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_age(17.5);
     }
-    */
+
+    public function testSetterWithInvalidAge2()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Age must be a non-negative integer!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_age(-35);
+    }
+
+    public function testSetterWithInvalidAge3()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Age must be a non-negative integer!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_age(TRUE);
+    }
+
+    public function testSetterWithInvalidAge4()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Age must be a non-negative integer!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_age("Goodbye World");
+    }
+
+    public function testSetterWithInvalidAge5()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Age must be a non-negative integer!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_age(["First Program" => "Hello World"]);
+    }
+
+    public function testSetterWithInvalidOccupation1()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Occupation must be a string!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_occupation(7);
+    }
+
+    public function testSetterWithInvalidOccupation2()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Occupation must be a string!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_occupation(0);
+    }
+
+    public function testSetterWithInvalidOccupation3()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Occupation must be a string!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_occupation(
+            (object)[1, 2, 3, 4, 5, "Last Program" => "Goodbye World"]
+        );
+    }
+
+    public function testSetterWithInvalidOccupation4()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Occupation must be a string!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_occupation(FALSE);
+    }
+
+    public function testSetterWithInvalidOccupation5()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Occupation must be a string!');
+        $person = new Person6("Donald Leung", 17, "Computer Programmer");
+        $person->set_occupation(TRUE);
+    }
 }
